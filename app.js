d769acf62501f1778484654c68a616b8c3b2d231
Travis Caro <travisbcaro@gmail.com>
2017-03-28 18:30:12 -0400

    Add arrow functions to fix closure issues

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 function App() {
+  this.elements = { };
+  this.context = null;
+  this.simulation = null;
+
   this.elements.canvas = $('#canvas')[0]; // TBC : Unwrap canvas from jquery wrapper
   this.elements.txtParticleCount = $('#txt-particle-count');
   this.elements.btnStart = $('#btn-start');
@@ -8,41 +12,66 @@ function App() {
   this.elements.txtYAcceleration = $('#txt-y-acceleration');
 
   this.context = this.elements.canvas.getContext('2d');
+  this.simulation = new Simulation();
 
   this.bindEventHandlers();
 }
 App.prototype = {
-  elements: { },
-  context: null,
-  simulation: null,
   bindEventHandlers() {
-    this.elements.btnStart.on('click', function() {
+    this.elements.btnStart.on('click', () => { // TBC : Use arrow function to maintain correct reference of 'this'
       console.log('start clicked...');
+      this.simulation.start();
     });
-    this.elements.btnStop.on('click', function() {
+    this.elements.btnStop.on('click', () => { // TBC : Use arrow function to maintain correct reference of 'this'
       console.log('stop clicked...');
+      this.simulation.stop();
     });
-    this.elements.btnReload.on('click', function() {
+    this.elements.btnReload.on('click', () => { // TBC : Use arrow function to maintain correct reference of 'this'
       console.log('reload clicked...');
+      this.simulation.reload();
+    });
+    $(document).on('simulation-tick', () => { // TBC : Use arrow function to maintain correct reference of 'this'
+      console.log('simulation-tick...');
     });
   },
 };
 
-function Simulation() { }
+function Simulation() {
+  this.particles = [];
+  this.obstacles = [];
+  this.started = false;
+  this.running = false;
+}
 Simulation.prototype = {
-  particles: [],
-  obstacles: [],
-  started: false,
-  running: false,
   start() {
     this.started = true;
-    // TODO
+    this.running = true;
+    window.requestAnimationFrame(() => { this.run(); }); // TBC : Use arrow function to maintain correct reference of 'this'
+  },
+  reload() {
+    this.particles = [];
+    this.obstacles = [];
+    this.started = false;
+    this.running = false;
+  },
+  stop() {
+    this.running = false;
+  },
+  run() {
+    if (this.started && this.running) {
+      this.tick();
+      window.requestAnimationFrame(() => { this.run(); }); // TBC : Use arrow function to maintain correct reference of 'this'
+    }
   },
   tick() {
     // TODO
     // TBC : Update particle position
     // TBC : Check collisions
       // TBC : If collided, bounce, stick, etc.
+    var event = jQuery.Event('simulation-tick');
+    event.particles = this.particles;
+    event.obstacles = this.obstacles;
+    $(document).trigger(event);
   },
   addParticle(particle) {
     this.particles.push(particle);
@@ -64,8 +93,24 @@ Simulation.prototype = {
   },
 };
 
-function Particle() { }
-Particle.prototype = { };
+function Particle(options) {
+  var DEFAULT_POSITION = { x:0, y:0 },
+      DEFAULT_VELOCITY = { xv:0, yv:0 },
+      DEFAULT_COLOR = '#fff',
+      DEFAULT_SIZE = 5,
+      DEFAULT_SHAPE = Particle.Shapes.CIRCLE;
+  options.hasOwnProperty('position') ? this.position = options.position : this.position = DEFAULT_POSITION;
+  options.hasOwnProperty('velocity') ? this.velocity = options.velocity : this.velocity = DEFAULT_VELOCITY;
+  options.hasOwnProperty('size') ? this.size = options.size : this.size = DEFAULT_SIZE;
+  options.hasOwnProperty('color') ? this.color = options.color : this.color = DEFAULT_COLOR;
+  options.hasOwnProperty('shape') ? this.color = options.color : this.color = DEFAULT_COLOR;
+}
+Particle.prototype = {
+  Shapes: {
+    CIRCLE: 'circle',
+    SQUARE: 'square'
+  }
+};
 
 function Obstacle() { }
 Obstacle.prototype = { };
